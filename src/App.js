3ce413074a6ef1b4187183b5d03a166ae0d3d5dc
sandypockets <zac@znacreative.com>
2021-10-19 20:21:05 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './main.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [url, setUrl] = useState('')
+
+  useEffect(() => {
+    // Get current tab url
+    const queryInfo = {active: true, lastFocusedWindow: true};
+    // eslint-disable-next-line no-undef
+    chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+      const url = tabs[0].url;
+      setUrl(url);
+    });
+  }, [])
+
   return (
     <div className="app">
-      <h1 className="get-started">Get started!</h1>
+      Current URL: <p className="get-started">{url}</p>
     </div>
   );
 }
