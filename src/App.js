ed43f93b875be0fabf35ae52f779487ba0057a5e
sandypockets <zac@znacreative.com>
2021-10-20 21:40:47 -0400

    Fix storage functionality

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [url, setUrl] = useState('')
-  const [time, setTime] = useState(0)
+  const [time, setTime] = useState()
 
   useEffect(() => {
     // Get current tab url
@@ -13,34 +13,47 @@ function App() {
       const url = tabs[0].url;
       setUrl(url);
     });
+
+    chrome.storage.local.get(['minutes'], function(result) {
+      console.log("RESULT", result)
+      setTime(result.minutes)
+    })
   }, [])
 
 
   chrome.alarms.onAlarm.addListener(function() {
-    setTime(time + 1)
+    setTime(time + 1);
+    chrome.storage.local.set({ minutes: time + 1 });
   })
 
   const handleOn = () => {
     function setAlarm() {
       chrome.action.setBadgeText({ text: 'ON' });
-      chrome.alarms.create({ delayInMinutes: 1 });
-      chrome.storage.sync.set({ minutes: 1 });
+      chrome.alarms.create({ delayInMinutes: 1, periodInMinutes: 1 });
+      chrome.storage.local.set({ minutes: time });
       window.close();
     }
     setAlarm()
   }
 
   const handleOff = () => {
-
+    function clearAlarm() {
+      chrome.action.setBadgeText({ text: '' });
+      chrome.alarms.clearAll();
+      window.close();
+    }
+    clearAlarm()
   }
   const clearTime = () => {
     setTime(0)
+    chrome.storage.local.set({ minutes: time });
   }
 
   return (
     <div className="app">
       Current URL: <p className="get-started">{url}</p>
-      <h1>{time}</h1>
+      <h1>Elapsed time</h1>
+      <h2>{time ? time : 0} minutes</h2>
       <button onClick={handleOn}>
         Turn on
       </button>
