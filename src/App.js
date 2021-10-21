950d98995945f490635322a780afe410996f79f0
sandypockets <zac@znacreative.com>
2021-10-20 22:47:28 -0400

    Add pages, separate concerns

--- src/App.js
+++ src/App.js
@@ -1,68 +1,67 @@
 import './main.css';
 import { useState, useEffect } from "react";
+import Home from "./components/Home";
+import Settings from "./components/Settings";
 
 function App() {
   const [url, setUrl] = useState('')
   const [time, setTime] = useState()
+  const [page, setPage] = useState('home')
 
   useEffect(() => {
-    // Get current tab url
-    const queryInfo = {active: true, lastFocusedWindow: true};
-    // eslint-disable-next-line no-undef
-    chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
-      const url = tabs[0].url;
-      setUrl(url);
-    });
-
     chrome.storage.local.get(['minutes'], function(result) {
       console.log("RESULT", result)
       setTime(result.minutes)
     })
   }, [])
 
+  function setAlarm() {
+    chrome.action.setBadgeText({ text: 'ON' });
+    chrome.alarms.create({ delayInMinutes: 1, periodInMinutes: 1 });
+    chrome.storage.local.set({ minutes: time });
+    window.close();
+  }
 
-  chrome.alarms.onAlarm.addListener(function() {
-    setTime(time + 1);
-    chrome.storage.local.set({ minutes: time + 1 });
-  })
+  function clearAlarm() {
+    setTime(0)
+    chrome.action.setBadgeText({ text: '' });
+    chrome.alarms.clearAll();
+    chrome.storage.local.set({ minutes: 0 });
+    window.close();
+  }
+
+  function pause() {
+    chrome.action.setBadgeText({ text: '!' });
+    chrome.alarms.clearAll();
+    window.close();
+  }
+
+
+  // chrome.alarms.onAlarm.addListener(function() {
+  //   setTime(time + 1);
+  //   chrome.storage.local.set({ minutes: time + 1 });
+  // })
 
   const handleOn = () => {
-    function setAlarm() {
-      chrome.action.setBadgeText({ text: 'ON' });
-      chrome.alarms.create({ delayInMinutes: 1, periodInMinutes: 1 });
-      chrome.storage.local.set({ minutes: time });
-      window.close();
-    }
     setAlarm()
   }
 
   const handleOff = () => {
-    function clearAlarm() {
-      chrome.action.setBadgeText({ text: '' });
-      chrome.alarms.clearAll();
-      window.close();
-    }
     clearAlarm()
   }
-  const clearTime = () => {
-    setTime(0)
-    chrome.storage.local.set({ minutes: time });
+  const handlePause = () => {
+    pause()
+  }
+
+  const handleReset = () => {
+    clearAlarm()
+    setAlarm()
   }
 
   return (
     <div className="app">
-      Current URL: <p className="get-started">{url}</p>
-      <h1>Elapsed time</h1>
-      <h2>{time ? time : 0} minutes</h2>
-      <button onClick={handleOn}>
-        Turn on
-      </button>
-      <button onClick={handleOff}>
-        Turn off
-      </button>
-      <button onClick={clearTime}>
-        Clear
-      </button>
+      {page === 'home' && <Home time={time} url={url} handlePause={handlePause} handleOff={handleOff} handleOn={handleOn} setPage={setPage} handleReset={handleReset} />}
+      {page === 'settings' && <Settings setPage={setPage} />}
     </div>
   );
 }
