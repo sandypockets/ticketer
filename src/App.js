c2060c370e95f60328d715595c6f8450efeaceeb
sandypockets <zac@znacreative.com>
2021-10-20 20:44:39 -0400

    Add simple alarm and listener

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [url, setUrl] = useState('')
+  const [time, setTime] = useState(0)
 
   useEffect(() => {
     // Get current tab url
@@ -14,9 +15,41 @@ function App() {
     });
   }, [])
 
+
+  chrome.alarms.onAlarm.addListener(function() {
+    setTime(time + 1)
+  })
+
+  const handleOn = () => {
+    function setAlarm() {
+      chrome.action.setBadgeText({ text: 'ON' });
+      chrome.alarms.create({ delayInMinutes: 1 });
+      chrome.storage.sync.set({ minutes: 1 });
+      window.close();
+    }
+    setAlarm()
+  }
+
+  const handleOff = () => {
+
+  }
+  const clearTime = () => {
+    setTime(0)
+  }
+
   return (
     <div className="app">
       Current URL: <p className="get-started">{url}</p>
+      <h1>{time}</h1>
+      <button onClick={handleOn}>
+        Turn on
+      </button>
+      <button onClick={handleOff}>
+        Turn off
+      </button>
+      <button onClick={clearTime}>
+        Clear
+      </button>
     </div>
   );
 }
