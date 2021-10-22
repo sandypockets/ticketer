4f499044c00ad13d6f126d6989118de9f0093c0e
sandypockets <zac@znacreative.com>
2021-10-21 21:51:36 -0400

    Add basic styling, onClick

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,44 +1,57 @@
-const handleLinkGroupClick = () => {
+const openTabGroupOne = () => {
   chrome.runtime.sendMessage("openTabGroupOne");
+}
+const openTabGroupTwo = () => {
+  chrome.runtime.sendMessage("openTabGroupTwo");
 }
 
 export default function Home({ ticketCount, increaseTicketCount, decreaseTicketCount, time, handleOn, handleOff, handlePause, setPage, handleReset }) {
   return (
-    <>
-      <h1>Elapsed time</h1>
-      <h2>{time ? time : 0} minutes</h2>
-      <button onClick={handleOn}>
-        Turn on
-      </button>
-      <button onClick={handleOff}>
-        Turn off
-      </button>
-      <button onClick={handlePause}>
-        Pause
-      </button>
-      <button onClick={handleReset}>
-        Reset
-      </button>
+    <div className="container">
       <div>
-        <button onClick={decreaseTicketCount}>
-          -
-        </button>
-        <span>Ticket count: {ticketCount} </span>
-        <button onClick={increaseTicketCount}>
-          +
-        </button>
-      </div>
-      <div>
-        <button onClick={handleLinkGroupClick}>
-          Open link group 1
-        </button>
-        <button>
-          Open link group 2
-        </button>
+        <div>
+          <h1>Productivity Title</h1>
+          <h3>Elapsed time</h3>
+          <h2>{time ? time : 0} minutes</h2>
+        </div>
+        <div>
+          <button onClick={handleOn}>
+            Turn on
+          </button>
+          <button onClick={handleOff}>
+            Turn off
+          </button>
+          <button onClick={handlePause}>
+            Pause
+          </button>
+          <button onClick={handleReset}>
+            Reset
+          </button>
+        </div>
+        <div>
+          <h3>Tickets completed</h3>
+          <div>
+            <button onClick={decreaseTicketCount}>
+              -
+            </button>
+            <span>Ticket count: {ticketCount} </span>
+            <button onClick={increaseTicketCount}>
+              +
+            </button>
+          </div>
+        </div>
+        <div>
+          <button onClick={openTabGroupOne}>
+            Open link group 1
+          </button>
+          <button onClick={openTabGroupTwo}>
+            Open link group 2
+          </button>
+        </div>
       </div>
       <div>
         <a onClick={() => setPage('settings')}>Settings</a>
       </div>
-    </>
+    </div>
   )
 }
