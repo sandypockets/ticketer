1a88371a80f5c65a2031f9cf99bc013eb96e6a64
sandypockets <zac@znacreative.com>
2021-10-21 22:40:24 -0400

    Add styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,46 +9,63 @@ export default function Home({ ticketCount, increaseTicketCount, decreaseTicketC
   return (
     <div className="container">
       <div>
-        <div>
-          <h1>Productivity Title</h1>
-          <h3>Elapsed time</h3>
-          <h2>{time ? time : 0} minutes</h2>
-        </div>
-        <div>
-          <button onClick={handleOn}>
-            Turn on
-          </button>
-          <button onClick={handleOff}>
-            Turn off
-          </button>
-          <button onClick={handlePause}>
-            Pause
-          </button>
-          <button onClick={handleReset}>
-            Reset
-          </button>
-        </div>
-        <div>
-          <h3>Tickets completed</h3>
+
+        <h1 className="title">Productivity Title</h1>
+        <div className="card">
           <div>
-            <button onClick={decreaseTicketCount}>
-              -
+            <div className="elapsedTime">
+              <h3>Elapsed time</h3>
+              <h2>{time ? time : 0} minutes</h2>
+            </div>
+          </div>
+          <div>
+            <button onClick={handleOn}>
+              Turn on
             </button>
-            <span>Ticket count: {ticketCount} </span>
-            <button onClick={increaseTicketCount}>
-              +
+            <button onClick={handleOff}>
+              Turn off
+            </button>
+            <button onClick={handlePause}>
+              Pause
+            </button>
+            <button onClick={handleReset}>
+              Reset
             </button>
           </div>
         </div>
-        <div>
-          <button onClick={openTabGroupOne}>
-            Open link group 1
-          </button>
-          <button onClick={openTabGroupTwo}>
-            Open link group 2
-          </button>
+
+        <div className="card">
+          <h3 className="flex-center">Tickets completed</h3>
+          <div className="buttonGroup">
+            <div>
+              <button onClick={decreaseTicketCount}>
+                -
+              </button>
+              <span>Ticket count: {ticketCount} </span>
+              <button onClick={increaseTicketCount}>
+                +
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
+
+        <div className="card">
+        <div className="flex-center">
+          <h3 className="quickTabs">Quick tabs</h3>
+        </div>
+          <p className="flex-center">Open three frequently used resources at once. Change them in Settings.</p>
+          <div className="buttonGroup">
+            <div>
+              <button onClick={openTabGroupOne}>
+                Group 1
+              </button>
+              <button onClick={openTabGroupTwo}>
+                Group 2
+              </button>
+            </div>
+          </div>
+        </div>
+        </div>
       <div>
         <a onClick={() => setPage('settings')}>Settings</a>
       </div>
